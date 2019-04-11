0ff05019627e92b08d0f32315b479cec148ba67b
Sebastian De Ro <Sebastian.DeRo@twinformatics.at>
2019-04-11 10:19:07 +0200

    starting point finalized

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,38 @@
-
+import {BehaviorSubject, from, of, Subject, timer} from "rxjs";
+import {tap, map, switchMap} from "rxjs/operators"
 
 // stream from array
 
-import {from, of} from "rxjs";
-import {tap, map, switchMap} from "rxjs/operators"
-
 const dataArray = ['a', 'b', 'c', 'd', 'e', 'f'];
 
-const stream = from(dataArray)
+const letter$ = from(dataArray)
 
-stream.pipe(
-    tap(value => console.log(`before filter ${value}`))
+letter$.pipe(
+    tap(letter => console.log(`before filter ${letter}`))
 )
+
+// stream from one value
+
+const point = 'a'
+
+const point$ = of(point);
+
+point$.pipe(
+    tap(point => console.log(`start of pipe: ${point}`))
+);
+
+
+// subjects
+
+const pet$ = new Subject()
+
+
+// behavior subject
+
+const isDisabled$ = new BehaviorSubject(false);
+
+
+// timed observable
+
+// starts after 1000 ms and emits every 1000ms
+const number$ = timer(1000, 1000);
